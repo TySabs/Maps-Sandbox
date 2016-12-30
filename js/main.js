59ba2facc23b322ac748da15ffcff5f8e8113bf7
Tyler Saballus <tysabs@gmail.com>
2016-12-30 12:35:14 -0600

    Add search input to search specific areas

--- js/main.js
+++ js/main.js
@@ -112,14 +112,19 @@ function initMap() {
     {title: 'Cloud Gate (The Bean)', location: {lat: 41.8827, lng: -87.6233}},
     {title: 'Buckingham Fountain', location: {lat: 41.8758, lng: -87.6189}},
     {title: 'Field Museum of Natural History', location: {lat: 41.8663, lng: -87.6170}},
+    {title: 'Museum of Science and Industry', location: {lat: 41.7906, lng: -87.5830}},
     {title: 'Navy Pier', location: {lat: 41.8917, lng: -87.6063}},
     {title: 'Art Institute of Chicago', location: {lat: 41.8796, lng: -87.6237}},
     {title: 'Shedd Aquarium', location: {lat: 41.8676, lng: -87.6140}},
+    {title: 'Adler Planetarium', location: {lat: 41.8663, lng: -87.6068}},
     {title: 'Willis Tower (Sears Tower)', location: {lat: 41.8789, lng: -87.6359}},
     {title: 'Wrigley Field', location: {lat: 41.9484, lng: -87.6553}},
     {title: 'Soldier Field', location: {lat: 41.8623, lng: -87.6167}},
     {title: 'United Center', location: {lat: 41.8807, lng: -87.6742}},
     {title: 'Guaranteed Rate Field (Comiskey)', location: {lat: 41.8299, lng: -87.6338}},
+    {title: 'Water Tower Place', location: {lat: 41.8979, lng: -87.6229}},
+    {title: 'Crown Fountain', location: {lat: 41.8815, lng: -87.6237}},
+    {title: 'McCormick Place', location: {lat: 41.8512, lng: -87.6170}},
     {title: 'Lincoln Park Zoo', location: {lat: 41.9211, lng: -87.6340}}
   ];
 
@@ -179,6 +184,10 @@ function initMap() {
   // Create click event to turn on drawing tools
   document.getElementById('toggle-drawing').addEventListener('click', function() {
     toggleDrawing(drawingManager);
+  });
+
+  document.getElementById('zoom-to-area').addEventListener('click', function() {
+    zoomToArea();
   });
 
   drawingManager.addListener('overlaycomplete', function(event) {
@@ -307,4 +316,32 @@ function searchWithinPolygon() {
       markers[i].setMap(null);
     }
   }
+}
+
+// Locate the input value and zoom into that area
+function zoomToArea() {
+  // Initialize the geocoder
+  var geocoder = new google.maps.Geocoder();
+  // Get the address or place that the user entered
+  var address = document.getElementById('zoom-to-area-text').value;
+  // Make sure address is not blank
+  if (address === '') {
+    window.alert('You must enter an area, or address.');
+  } else {
+    // Geocode the address entered to get the center. Then center the map
+    // and zoom in on this area
+    geocoder.geocode(
+      { address : address,
+        componentRestrictions: {locality: 'Chicago'}
+      }, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          map.setZoom(15);
+        } else {
+          window.alert('We could not find that location - try entering a more' +
+            ' specific place.');
+        }
+      });
+  }
+
 }
